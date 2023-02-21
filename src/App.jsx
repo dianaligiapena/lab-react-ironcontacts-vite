8bae1ba29d63dc4d07a8178fd4d0cdcb2e2720b1
Diana Pena <dianaligiapena@gmail.com>
2023-02-21 18:21:58 +0200

    3rd iteration

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,27 @@
 import './App.css'
 import contactsJSON from "./contacts.json"
+import { useState } from "react";
 
 function App() {
-  const firstSix = contactsJSON.slice(0, 6);
-  console.log(firstSix);
+  const [ contacts, setContacts ] = useState(contactsJSON.slice(0, 2));
 
+  const [ remaining, setRemaining  ] = useState(contactsJSON.slice(6));
+
+  const addRandomContact = () => {
+    const random = Math.floor(Math.random() * remaining.length);
+
+    const newContact = remaining.splice(random, 1)[0];
+    
+    console.log(remaining.length);
+    setContacts( [ ...contacts, newContact ]  );
+  }
 
   return <div className='App'>
     <h1>IronContacts</h1>
+
+    <div>
+      <button onClick={addRandomContact}>Add Random Contact</button>
+    </div>
 
     <table>
       <thead>
@@ -21,7 +35,7 @@ function App() {
       </thead>
 
       <tbody>
-        {firstSix.map((element) => {
+        {contacts.map((element) => {
             return (
               <tr key={element.id} className="table">
                 <td>
