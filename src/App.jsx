4748201ae5ee3d4e67a1faf11c602fb5e3c41af1
Diana Pena <dianaligiapena@gmail.com>
2023-02-21 18:57:35 +0200

    iteration 4

--- src/App.jsx
+++ src/App.jsx
@@ -3,24 +3,33 @@ import contactsJSON from "./contacts.json"
 import { useState } from "react";
 
 function App() {
-  const [ contacts, setContacts ] = useState(contactsJSON.slice(0, 2));
-
-  const [ remaining, setRemaining  ] = useState(contactsJSON.slice(6));
+  const [ contacts, setContacts ] = useState(contactsJSON.slice(0, 5));
+  const [ remaining, setRemaining  ] = useState(contactsJSON.slice(5));
 
   const addRandomContact = () => {
     const random = Math.floor(Math.random() * remaining.length);
-
     const newContact = remaining.splice(random, 1)[0];
-    
     console.log(remaining.length);
     setContacts( [ ...contacts, newContact ]  );
   }
+
+  const sortByPopularity = () => {
+    const popularityList = [...contacts].sort((a, b) => b.popularity - a.popularity);
+    setContacts(popularityList);
+  };
+
+  const sortByName = () => {
+    const nameList = [...contacts].sort((a, b) => a.name.localeCompare(b.name));
+    setContacts(nameList);
+  };
 
   return <div className='App'>
     <h1>IronContacts</h1>
 
     <div>
       <button onClick={addRandomContact}>Add Random Contact</button>
+      <button onClick={sortByPopularity}>Sort By Popularity</button>
+      <button onClick={sortByName}>Sort By Name</button>
     </div>
 
     <table>
